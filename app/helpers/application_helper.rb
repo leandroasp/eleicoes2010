ac82f6364cf00c5f1b3f2b351da13999f4c99c07
Cleiton Francisco <cleitonfco@gmail.com>
2010-10-02 21:51:05 -0300

    Helper para montar o cargo no topo da página

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def mostra_cargo
+    html = @resultado['cargo']
+    html << " / #{@abrangencia['codigoAbrangencia']}" if @resultado['cargo'] != "Presidente"
+    html
+  end
 end
