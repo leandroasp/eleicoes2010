69b88a73a23a4153194aa762cd8ea88467841444
Cleiton Francisco <cleitonfco@gmail.com>
2010-10-03 08:06:50 -0300

    Helper "Outros Cargos"

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,14 @@ module ApplicationHelper
     html << " / #{@abrangencia['codigoAbrangencia']}" if @resultado['cargo'] != "Presidente"
     html
   end
+
+  def outros_cargos(atual)
+    cargos = [['Presidente', 1], ['Governador', 3], ['Senador', 5], ['Dep. Federal', 6], ['Dep. Estadual', 7]]
+    html = %{<ul id="cargos"><li class="select">Selecione um cargo:</li>}
+    cargos.each do |cargo|
+      html << %{<li>#{link_to(cargo[0], result_url('pi', cargo[1]))}</li>} # unless cargo[0] == atual
+    end
+    html << "</ul>"
+    html
+  end
 end
